cead58c587b08cc8e39423b27ee72458885b1e76
VICTOR EDUARDO DIAS DUTRA <victor.eddutra@senacsp.edu.br>
2025-11-06 17:00:25 -0300

    fix|tarefaRoutes: resolvendo erro de sixtaxe em router.post #1

--- src/routes/tarefaRoutes.ts
+++ src/routes/tarefaRoutes.ts
@@ -5,7 +5,7 @@ const router = Router();
 //Metodos CRUD para as rotas | GET/POST/PUT/DELETE
 router.get("/", ()=>console.log('pegar todas as tarefas'))
 router.get("/:id", ()=>console.log('pegar uma tarefa'))
-router.post("/:", ()=>console.log('cadastrar uma tarefa'))
+router.post("/", ()=>console.log('cadastrar uma tarefa'))
 router.put("/:id", ()=>console.log('atualizar uma tarefa'))
 router.delete("/:id", ()=>console.log('deletar uma tarefa'))
 
